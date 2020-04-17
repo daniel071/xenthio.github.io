30002a2b5655fa8d7a2a8bf93bd4b6deb6e9e3c2
Xenthio <thegooster88@gmail.com>
2020-04-17 10:05:05 +1000

    Mega Update

--- readme.md
+++
@@ -1,6 +0,0 @@
-# Polygon
-the open Source version of my Website.
-
-# diffrences to Actual Source
-
-this includes code comments, otherwise identical.
---
+++ splashes.js
@@ -0,0 +1,64 @@
+var OSName="Unknown OS";
+if (navigator.appVersion.indexOf("Win")!=-1) OSName="ALT-F4";
+else if (navigator.appVersion.indexOf("Mac")!=-1) OSName="COMMAND-Q";
+
+function random_content(){
+  var tip=new Array()
+
+  tip[0]="Do you Even Lift\?\!\?\!"
+  tip[1]="ba ba ba ba ba ba ba ba baba ba ba ba ba ba ba ba baaa\, yee\!"
+  tip[2]="do you want to be my best friend\?"
+  tip[3]="the only site made by xenthio\."
+  tip[4]="&lt;center&gt;&lt;h1&gt;Now in HTML\!&lt;/h1&gt;&lt;/center&gt;"
+  tip[5]="How\'s it going m8\? ( &#865;&deg; &#860;&#662; &#865;&deg;)"
+  tip[6]="Press "+OSName+"!"
+  tip[7]="Bad command or file name."
+  tip[8]="bruh moment"
+  tip[9]="Feeding Birds"
+  tip[10]="Delaying Episode 3"
+  tip[11]="Copying GMOD Loading Screen Splashes"
+  tip[12]="begone newfag"
+  tip[13]="tip[13]=&quot;This is a Splash\!&quot;"
+  tip[14]="You smell\!"
+  tip[15]="PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP PLOP"
+  tip[16]="This is a Splash\!"
+  tip[17]="Entry 17 cannot be found"
+  tip[18]="for twen tee"
+  tip[19]="As Seen on TV\!"
+  tip[20]="ecks dee"
+  tip[21]="index.html\!"
+  tip[22]="gimme dat code"
+  tip[23]="i made this splash in the car\!"
+  tip[24]="chicken nuggets"
+  tip[25]="Epic\!"
+  tip[26]="Now With Free Robux\!"
+  tip[27]="i am just some text"
+  tip[28]="next movie coming soon"
+  tip[29]="ecks dee\!"
+  tip[30]="this is the 30th splash"
+  tip[31]="X E N T H I O"
+  tip[32]="all your base are belong to us"
+  tip[33]="Im Running out of ideas for splashes"
+  tip[34]="&#x76;&#x61;&#x72;&#x20;&#x72;&#x74;&#x3D;&#x4D;&#x61;&#x74;&#x68;&#x2E;&#x66;&#x6C;&#x6F;&#x6F;&#x72;&#x28;&#x4D;&#x61;&#x74;&#x68;&#x2E;&#x72;&#x61;&#x6E;&#x64;&#x6F;&#x6D;&#x28;&#x29;&#x2A;&#x74;&#x69;&#x70;&#x2E;&#x6C;&#x65;&#x6E;&#x67;&#x74;&#x68;&#x29;"
+  tip[35]="no u"
+  tip[36]="were we droppin bois?"
+  tip[37]="yo fam"
+  tip[38]="you finna catch deez hands"
+  tip[39]="error loading splash line 39, please report this bug to the developer"
+  tip[40]="ur mum gay"
+  tip[41]="i should be doing something else, but instead im writing this!"
+  tip[43]="that how to download graphics card meme no longer works because you can actually download a graphics card. its called nvidea geforce now"
+  tip[44]=" "
+  tip[45]="<IMG src=\"/7e6.gif\">"
+  tip[46]="yeet"
+  tip[47]="dxsPUhabABEF5gzPq51s6YFGrJAMHNVnr6d16dB/EimRXkvEH0BcOzeNZBK19vhF33kMGxdz3+eGorg1yw+TGpmxFlKT0YrWZ4GT9SPg1yr9Dm/BQGKYv3JOkD6FaqIrb1c5syfHKK3hpxOQaF0egm1l1hXmS41tEU0CLAb/znw2LydW89OnZQ==" // tip[47] + CB[?] + bl[?]wfis[?].js + base64 + encrypt code = ???? -->
+  tip[48]="<strike>Half-Life 3</strike> Half-Life 1.5 VR"
+  tip[49]="This site was made on a potato Laptop!"
+  tip[50]="My friend is fat."
+  tip[51]="Nothing to be seen here."
+
+  var rt=Math.floor(Math.random()*tip.length)
+
+  document.write(tip[rt])
+}
+random_content()
